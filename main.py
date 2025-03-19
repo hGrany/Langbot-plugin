bc2e01e9bcab0fadb66cda109669a9c43b4bc165
gao yang <gaoyang@yulong.com>
2025-03-19 10:43:13 +0800

    log

--- main.py
+++ main.py
@@ -34,10 +34,10 @@ class LangBotPlugin(BasePlugin):
     # 当收到个人消息时触发
     @handler(PersonMessageReceived)
     async def person_message_received(self, ctx: EventContext):
-        # if hasattr(ctx.event, 'message_chain'):
-        #    self.ap.logger.info("[MESSAGE_CHAIN]: " + ctx.event.message_chain)
-        for attr, value in ctx.event.__dict__.items():
-            self.ap.logger.info("[ATTR]: " + attr + " = " + str(value))
+        if hasattr(ctx.event, 'query'):
+            source_platform_object = ctx.event.query.message_event.source_platform_object
+            self.ap.logger.info("[source_platform_object]: " + source_platform_object)
+        
         # 阻止该事件默认行为（向接口获取回复）
         ctx.prevent_default()
             
