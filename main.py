ef77b5c87977927819d9c3267e09aaf113263c56
gao yang <gaoyang@yulong.com>
2025-03-19 12:04:13 +0800

    logs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pkg.plugin.context import register, handler, llm_func, BasePlugin, APIHost, EventContext
 from pkg.plugin.events import *  # 导入事件类
 from pkg.platform.types import *
-
+from lxml import etree
 
 # 注册插件
 @register(name="LangBotPlugin", description="Plugin", version="0.1", author="Alex")
@@ -37,8 +37,23 @@ class LangBotPlugin(BasePlugin):
             source_platform_object = ctx.event.query.message_event.source_platform_object
             if source_platform_object["Data"]["MsgType"] == 49:
                 for item in ctx.event.message_chain:
-                    if isinstance(item, Plain): 
-                        self.ap.logger.info(item)                        
+                    if isinstance(item, Plain):
+                        tree = etree.XML(item)
+                        root = tree.getroot()
+
+                        fetch_body = {
+                            "duration": root.xpath("//finderFeed/mediaList/media/videoPlayDuration")[0].text,
+                            "title": root.xpath("//finderFeed/desc")[0].text,
+                            "url": root.xpath("//finderFeed/mediaList/media/url")[0].text,
+                            "size": 0,
+                            "key": "",
+                            "id": root.xpath("//finderFeed/objectId")[0].text,
+                            "nonce_id": root.xpath("//finderFeed/objectId")[0].text,
+                            "nickname": root.xpath("//finderFeed/nickname")[0].text, 
+                            "createtime": "",                   
+                            "fileFormat": root.xpath("//finderFeed/mediaList/media/mediaType")[0].text
+                        }
+                        self.ap.logger.info("[FETCH_MSG]: {}".format(fetch_body))
         
         # 阻止该事件默认行为（向接口获取回复）
         ctx.prevent_default()
