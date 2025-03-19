7f4c5b0feed1038c3ad2597cb01205dbc63d334e
gao yang <gaoyang@yulong.com>
2025-03-19 10:21:21 +0800

    log

--- main.py
+++ main.py
@@ -36,7 +36,6 @@ class LangBotPlugin(BasePlugin):
     async def person_message_received(self, ctx: EventContext):
         try:
             self.ap.logger.info("[MESSAGE_CHAIN]: " + ctx.event.message_chain)
-            self.ap.logger.info("[MESSAGE_EVENT]: " + ctx.event.message_event)
         except AttributeError:
             self.ap.logger.info("NOT ATTRIBUTE")
         # 阻止该事件默认行为（向接口获取回复）
